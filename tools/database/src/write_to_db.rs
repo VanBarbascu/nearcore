db336c7b4d5de770d1e1dd4166e140c7bc0d498d
Razvan Barbascu <razvan@nearone.org>
2025-08-20 18:31:54 +0100

    add db col ammend cmd

--- tools/database/src/write_to_db.rs
+++ tools/database/src/write_to_db.rs
@@ -1,11 +1,11 @@
-use near_chain_configs::GenesisValidationMode;
-use near_store::{DBCol, NodeStorage};
-use std::path::Path;
+use near_chain_configs::{GenesisValidationMode};
+use near_store::{DBCol, NodeStorage, Store};
+use std::path::{Path, PathBuf};
 
 #[derive(clap::Subcommand)]
 enum BlockMiscKeySelector {
     StateSnapshot,
-    HeaderHead
+    HeaderHead,
 }
 
 #[derive(clap::Subcommand)]
@@ -19,12 +19,36 @@ enum ColumnSelector {
 #[derive(clap::Args)]
 pub(crate) struct WriteCryptoHashCommand {
     #[clap(long)]
-    hash: near_primitives::hash::CryptoHash,
+    hash: Option<near_primitives::hash::CryptoHash>,
+    #[clap(long, conflicts_with = "hash")]
+    clone_from_other_home_dir: Option<PathBuf>,
     #[clap(subcommand)]
     column: ColumnSelector,
 }
 
 impl WriteCryptoHashCommand {
+
+    fn get_crypto_hash(&self, source_store: Option<&Store>, key: &[u8]) -> anyhow::Result<near_primitives::hash::CryptoHash> {
+        if let Some(hash) = self.hash {
+            return Ok(hash);
+        }
+        if let Some(source_store) = source_store {
+            if let Some(value) = source_store.get_ser::<near_primitives::hash::CryptoHash>(DBCol::BlockMisc, key)? {
+                return Ok(value);
+            }
+        }
+        anyhow::bail!("No source store provided and no hash provided");
+    }
+
+    fn get_tip(&self, source_store: Option<&Store>, key: &[u8]) -> anyhow::Result<near_primitives::block::Tip> {
+        if let Some(source_store) = source_store {
+            if let Some(value) = source_store.get_ser::<near_primitives::block::Tip>(DBCol::BlockMisc, key)? {
+                return Ok(value);
+            }
+        }
+        anyhow::bail!("No source store provided for Tip");
+    }
+
     pub(crate) fn run(
         &self,
         home_dir: &Path,
@@ -37,6 +61,18 @@ impl WriteCryptoHashCommand {
             near_config.config.archival_config(),
         );
 
+        let source_store = if let Some(clone_from_other_home_dir) = &self.clone_from_other_home_dir {
+            let source_near_config = nearcore::config::load_config(&clone_from_other_home_dir, GenesisValidationMode::UnsafeFast)?;
+            let source_opener = NodeStorage::opener(
+                clone_from_other_home_dir,
+                &source_near_config.config.store,
+                source_near_config.config.archival_config(),
+            );
+            let source_storage = source_opener.open()?;
+            let source_store = source_storage.get_hot_store();
+            Some(source_store)
+        } else { None };
+       
         let storage = opener.open()?;
         let store = storage.get_hot_store();
         let mut store_update = store.store_update();
@@ -47,14 +83,14 @@ impl WriteCryptoHashCommand {
                     store_update.set_ser(
                         DBCol::BlockMisc,
                         near_store::STATE_SNAPSHOT_KEY,
-                        &self.hash,
+                        &self.get_crypto_hash(source_store.as_ref(), near_store::STATE_SNAPSHOT_KEY)?,
                     )?;
                 }
                 BlockMiscKeySelector::HeaderHead => {
                     store_update.set_ser(
                         DBCol::BlockMisc,
                         near_store::HEADER_HEAD_KEY,
-                        &self.hash,
+                        &self.get_tip(source_store.as_ref(), near_store::HEADER_HEAD_KEY)?,
                     )?;
                 }
             },
